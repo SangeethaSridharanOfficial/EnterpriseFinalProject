981cfc50750eaaf474c0925e7c68dc14651a10d2
SangeethaSridharan <sangeethasridharan93@gmail.com>
2022-04-22 12:57:44 -0400

    Fixed delete functionality

--- src/main/java/com/project/endpoints/BookEndPoint.java
+++ src/main/java/com/project/endpoints/BookEndPoint.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping(path={"/books", "/page/books"})
 public class BookEndPoint {
 
 
